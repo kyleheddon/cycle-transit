be26e2401cd325cfae6a3a29b7c4c32ad7bf7157
Kyle Heddon <kheddon@salesforce.com>
2019-11-16 07:28:29 -0500

    hack to fix location autocomplete icon issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+
+/*
+ * BEGIN HACK
+ * TextField must be imported before Autocomplete for unknown reasons.
+ * If Autocomplete is imported first, the endAdornment icon gets placed
+ * below the TextField. In order to fix this across the entire app,
+ * the imports have been moved to this root level component.
+*/
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+/* END HACK */
+
 import { Route, Switch } from 'react-router-dom'; 
 import Home from './Home';
 
